da5f4c9500a65b8e541ed9086223c00b1921d645
VONGOCTIEN <vntien2310@gmail.com>
2021-12-24 20:10:19 +0700

    timsonguyentotrongmang

---
+++ Desktop/In số nguyên tố trong mảng/tìm số nguyên tố trong mảng/inmangvsnt/lambt.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<math.h>
+#define MAX 100
+/* hàm nhập các phần tử cho mảng */
+void main()
+{
+	int i;
+	int n;
+	int a[MAX];
+	printf("in ra gia tri can tim :");
+	scanf_s("%d", &n);
+	for (i = 0; i < n; i++) {
+		printf("a[%d] =", i);
+		scanf_s("%d", &a[i]);
+	}for (i = 0;i<n; i++) {
+		if (a[i] % 2 == 0) {
+			printf("%d la so chan \r\n", a[i]);
+
+		}
+		else
+			printf("%d la so le \r\n", a[i]);
+
+
+	}
+
+}
